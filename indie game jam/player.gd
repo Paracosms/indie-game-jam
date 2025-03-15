e5c802b8757620243243cde53b6a3b8998b04097
Paracosms <92565929+Paracosms@users.noreply.github.com>
2025-03-15 01:01:56 -0700

    init
    day 1
    - shotgun
    - player movement

---
+++ indie game jam/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var input = Vector2.ZERO
+const SPEED = 300
+
+# call movement function on each player frame
+func _physics_process(delta):
+	player_movement(delta)
+
+# find movement direction
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+
+# calculate movement and move the player
+func player_movement(delta):
+	velocity = get_input() * SPEED
+	move_and_slide()
+
+# 1 selects the attack (rock shotgun)
+  # shoots 5 pellets at a random angle from -pi/4 to pi/4
+# 2 selects the chop
